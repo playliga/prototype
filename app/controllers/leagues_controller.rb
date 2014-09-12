f2c985d3b087df98224223ea6ce316206f7215c3
lemonpole <noreply@website.com>
2014-09-12 14:28:14 -0400

    more testing finished on leagues controller.

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,10 +1,17 @@
 class LeaguesController < ApplicationController
+	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+	
 	def index
 		leagues_arr = League.all
 		render :json => leagues_arr
 	end
 	
 	def show
-		# TODO
+		league_obj = League.find(params[:id])
+		render :json => league_obj
+	end
+	
+	def record_not_found
+		render :json => {}
 	end
 end
--- test/controllers/leagues_controller_test.rb
+++ test/controllers/leagues_controller_test.rb
@@ -8,19 +8,19 @@ class LeaguesControllerTest < ActionController::TestCase
 		assert_response :success
 	end
 	test 'show sends league json response' do
-		get :show, :id => 1
+		get :show, { id: 1 }
 		league_obj = JSON.parse(@response.body)
 		assert league_obj['name']
 		assert_response :success
 	end
 	test 'show sends league json response with four divisions' do
-		get :show, :id => 1
+		get :show, { id: 1 }
 		league_obj = JSON.parse(@response.body)
 		assert_equal(4, league_obj.tourneys.size)
 		assert_response :success
 	end
 	test 'create should work' do
-		post :create, { :name => 'Ligue 1', :game_id => 2 }
+		post :create, { name: 'Ligue 1', game_id: 2 }
 		league_obj = JSON.parse(@response.body)
 		assert league_obj['name']
 		assert_response :success
