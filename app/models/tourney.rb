3f2547b6b76d0f316a7eab677e4e7dfa04b4f1df
lemonpole <tud13224@temple.edu>
2014-09-15 13:11:04 -0400

    completed some unit tests for tourney model.

--- app/models/tourney.rb
+++ app/models/tourney.rb
@@ -1,4 +1,12 @@
 class Tourney < ActiveRecord::Base
-	validates :name, length:{ maximum: 128 }
+	validates :name, uniqueness:true, length:{ maximum: 128 }
+	validates :in_stage, length:{ maximum: 32 }
+	validates :max_slots, :open_slots, presence:true
+	validates :max_slots, :open_slots, :season_num, numericality:{ only_integer:true }
+	validates :is_roundrobin, :is_autofill, exclusion:{ in: [true, false] }
+	validates :game, presence:true
+	
 	belongs_to :game
+	belongs_to :division
+	belongs_to :league
 end
--- test/models/tourney_test.rb
+++ test/models/tourney_test.rb
@@ -1,81 +1,107 @@
 require 'test_helper'
 
 class TourneyTest < ActiveSupport::TestCase
-	test 'name cannot be longer than 128 chars' do
+	test '1' do
 		t_obj = Tourney.new
 		t_obj.name = 'abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890'
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'name cannot be longer than 128 chars'
 	end
-	test 'in_stage cannot be longer than 32 chars' do
+	test '1b' do
+		t_obj = Tourney.new
+		t_obj.name = 'Name'
+		t_obj.open_slots = 32
+		t_obj.max_slots = 32
+		t_obj.game = Game.find(1)
+		t_obj.save
+		
+		alt_obj = Tourney.new
+		alt_obj.name = 'Name'
+		alt_obj.open_slots = 32
+		alt_obj.max_slots = 32
+		alt_obj.game = Game.find(1)
+		assert !alt_obj.save, 'tourney names should be unique'
+	end
+	test '2' do
 		t_obj = Tourney.new
 		t_obj.in_stage = 'abcdefghijklmnopqrstuvwxyz1234567890'
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'in_stage cannot be longer than 32 chars'
 	end
-	test 'max_slots cannot be empty' do
+	test '3' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 16
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'max_slots cannot be empty'
 	end
-	test 'open_slots cannot be empty' do
+	test '4' do
 		t_obj = Tourney.new
-		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'open_slots cannot be empty'
 	end
-	test 'max_slots must be an integer' do
+	test '5' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 32
 		t_obj.max_slots = 'A String'
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'max_slots must be an integer'
 	end
-	test 'open_slots must be an integer' do
+	test '6' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 'A String'
 		t_obj.max_slots = 32
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'open_slots must be an integer'
 	end
-	test 'season_num must be an integer' do
+	test '7' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.season_num = 'A String'
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'season_num must be an integer'
 	end
-	test 'is_autofill must be a boolean' do
+	test '8' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.is_autofill = 'A String'
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'is_autofill must be a boolean'
 	end
-	test 'is_roundrobin must be a boolean' do
+	test '9' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.is_roundrobin = 'A String'
 		t_obj.game = Game.find(1)
-		assert !t_obj.save
+		assert !t_obj.save, 'is_roundrobin must be a boolean'
 	end
-	test 'game association must be set' do
-		# do work.
+	test '10' do
+		t_obj = Tourney.new
+		t_obj.open_slots = 32
+		t_obj.max_slots = 32
+		assert !t_obj.save, 'tourney must have a game set'
 	end
-	test 'get the division name tourney is in' do
-		# do work.
+	test '11' do
+		t_obj = Tourney.new
+		t_obj.open_slots = 32
+		t_obj.max_slots = 32
+		t_obj.division = Division.find(1)
+		assert t_obj.division.name, 'tourney should get division name if found'
 	end
-	test 'get the league name tourney is in' do
-		# do work.
+	test '12' do
+		t_obj = Tourney.new
+		t_obj.open_slots = 32
+		t_obj.max_slots = 32
+		t_obj.league = League.find(1)
+		t_obj.division = Division.find(1)
+		assert t_obj.league.name, 'tourney should get league name if found'
 	end
 	test 'get the competitor names for this tourney' do
 		# do work.
