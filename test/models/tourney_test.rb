d1b36df6e08d9cd909032f457be2457c25465de7
Steve Rivera <srivera.x1@gmail.com>
2014-12-12 17:10:56 -0500

    refactoring of tests and fixtures

--- test/models/tourney_test.rb
+++ test/models/tourney_test.rb
@@ -6,7 +6,7 @@ class TourneyTest < ActiveSupport::TestCase
 		t_obj.name = 'abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890'
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'name cannot be longer than 128 chars'
 	end
 	test '1b' do
@@ -14,14 +14,14 @@ class TourneyTest < ActiveSupport::TestCase
 		t_obj.name = 'Name'
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		t_obj.save
 		
 		alt_obj = Tourney.new
 		alt_obj.name = 'Name'
 		alt_obj.open_slots = 32
 		alt_obj.max_slots = 32
-		alt_obj.game = Game.find(1)
+		alt_obj.game = games(:cstrike)
 		assert !alt_obj.save, 'tourney names should be unique'
 	end
 	test '2' do
@@ -29,33 +29,33 @@ class TourneyTest < ActiveSupport::TestCase
 		t_obj.in_stage = 'abcdefghijklmnopqrstuvwxyz1234567890'
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'in_stage cannot be longer than 32 chars'
 	end
 	test '3' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 16
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'max_slots cannot be empty'
 	end
 	test '4' do
 		t_obj = Tourney.new
 		t_obj.max_slots = 32
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'open_slots cannot be empty'
 	end
 	test '5' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 32
 		t_obj.max_slots = 'A String'
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'max_slots must be an integer'
 	end
 	test '6' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 'A String'
 		t_obj.max_slots = 32
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'open_slots must be an integer'
 	end
 	test '7' do
@@ -63,26 +63,10 @@ class TourneyTest < ActiveSupport::TestCase
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
 		t_obj.season_num = 'A String'
-		t_obj.game = Game.find(1)
+		t_obj.game = games(:cstrike)
 		assert !t_obj.save, 'season_num must be an integer'
 	end
-	test '8' do
-		t_obj = Tourney.new
-		t_obj.open_slots = 32
-		t_obj.max_slots = 32
-		t_obj.is_autofill = 'A String'
-		t_obj.game = Game.find(1)
-		assert !t_obj.save, 'is_autofill must be a boolean'
-	end
 	test '9' do
-		t_obj = Tourney.new
-		t_obj.open_slots = 32
-		t_obj.max_slots = 32
-		t_obj.is_roundrobin = 'A String'
-		t_obj.game = Game.find(1)
-		assert !t_obj.save, 'is_roundrobin must be a boolean'
-	end
-	test '10' do
 		t_obj = Tourney.new
 		t_obj.open_slots = 32
 		t_obj.max_slots = 32
