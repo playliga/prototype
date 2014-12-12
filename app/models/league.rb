d55b0961e956e43edc79ce96b464e461847fe70a
Steve Rivera <srivera.x1@gmail.com>
2014-12-11 22:44:12 -0500

    Added game_id to league table

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,7 @@
 class League < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true, length: { maximum: 64 }
 	validates :current_season, numericality: { only_integer:true }
+	
+	belongs_to :game, inverse_of: :leagues
 	has_many :tourneys, inverse_of: :league
 end
--- db/migrate/20140911025709_create_leagues.rb
+++ db/migrate/20140911025709_create_leagues.rb
@@ -3,6 +3,8 @@ class CreateLeagues < ActiveRecord::Migration
 		create_table :leagues do |t|
 			t.string :name, :null => false, :limit => 64
 			t.integer :current_season, :null => true, :default => 1
+			
+			t.belongs_to :game, :null => false
 			t.timestamps
 		end
 	end
