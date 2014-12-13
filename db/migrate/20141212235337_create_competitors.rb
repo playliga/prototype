05d18e4869c0428f1be3adb2cca14ee64ea4b6ca
Steve Rivera <srivera.x1@gmail.com>
2014-12-12 20:00:16 -0500

    finished tests for model

--- db/migrate/20141212235337_create_competitors.rb
+++ db/migrate/20141212235337_create_competitors.rb
@@ -1,7 +1,7 @@
 class CreateCompetitors < ActiveRecord::Migration
 	def change
 		create_table :competitors do |t|
-			t.string :alias, :limit => 64, :null => false
+			t.string :name, :limit => 64, :null => false
 			t.integer :group_num, :null => true
 			t.integer :wins, :null => true, :default => 0
 			t.integer :loss, :null => true, :default => 0
