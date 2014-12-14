906c72520545d26499af81f78d76b67575da7637
Steve Rivera <srivera.x1@gmail.com>
2014-12-14 14:55:09 -0500

    added stage constants to tourney model

--- app/models/tourney.rb
+++ app/models/tourney.rb
@@ -1,4 +1,9 @@
 class Tourney < ActiveRecord::Base
+	STAGE_BUILDING = 'Building'
+	STAGE_READY = 'Ready'
+	STAGE_WAITING = 'Waiting'
+	STAGE_COMPLETED = 'Completed'
+	
 	validates :name, uniqueness:true, length:{ maximum: 128 }, allow_blank: true
 	validates :in_stage, length:{ maximum: 32 }
 	validates :max_slots, :open_slots, presence:true
