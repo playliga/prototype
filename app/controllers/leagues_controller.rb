d904ddb93a362013c45c5c8063177e281cbc8f43
Steve Rivera <srivera.x1@gmail.com>
2015-01-05 13:49:14 -0500

    finished start league function

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -28,32 +28,41 @@ class LeaguesController < ApplicationController
 		#puts tourney_obj.to_yaml
 		render :json => league_obj, :status => :created
 	end
-
-=begin
-all divisions in league's current season should be set to ready status.
-generate the matches for each division.
-	only one group.
-	open: 8 games/player
-	IM/I: 16 games/player (faces opponent twice)
-create helper function that generates matches from a list of competitors.
-	optional arg of how many times they face each other.
-=end
 	def start
 		league_obj = League.find(params[:id])
 		tourney_arr = Tourney.where('league_id = ? AND season_num = ?', league_obj.id, league_obj.current_season)
 		
-		# abort if a tourney is found that is not ready
 		tourney_not_ready = tourney_arr.find {|s| s.in_stage != Tourney::STAGE_READY}
 		unless tourney_not_ready.nil?
 			render :json => 'Divisions found that are not yet ready', :status => :forbidden
 		end
 		
-		# loop through each division and generate matches.
-		# create helper function for generating matches. optional arg of how many times to face an opponent.
 		tourney_arr.each do |tourney_obj|
-			# if open, 8 games/player
-			# else 16 games/player (faces an opponent twice)
+			comps_arr = tourney_obj.competitors
+			comps_match_set = Array.new
+			
+			comps_arr.each do |competitor|
+				comps_match_set.push(competitor)
+				if comps_match_set.length % 9 == 0
+					num_times = tourney_obj.division.name == 'Open'? 1: 2
+					matches_arr = Tourney.gen_matches(comps_match_set, num_times)
+					
+					matches_arr.each do |match|
+						match_obj = Match.new
+						match_obj.tourney = tourney_obj
+						
+						comp_one = Competitor.where({tourney_id: tourney_obj.id, name: match[0]}).first
+						comp_two = Competitor.where({tourney_id: tourney_obj.id, name: match[1]}).first
+						
+						match_obj.competitors = [comp_one, comp_two]
+						match_obj.save
+					end
+					comps_match_set.clear
+				end
+			end
 		end
+		
+		render :json => 'League successfully started'
 	end
 	
 	def record_not_found
