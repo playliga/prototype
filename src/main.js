4bd16d89d9a8f1e79f12b96733cf59eb33f0f6a2
lemonpole <srivera.x1@gmail.com>
2015-07-14 19:49:30 -0400

    better documentation

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ var mainWindow = null; // prevent window being GC'd
 // report crashes to electron
 require('crash-reporter').start();
 
-// handle events
+// when the main window is closed
 app.on('window-all-closed', function() {
   // when closing, OSX has some issues so we manually close it below
   if (process.platform != 'darwin') {
@@ -13,6 +13,8 @@ app.on('window-all-closed', function() {
   }
 });
 
+// this is our main app window
+// you can specify different properties for it when instantiating
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600, center: true});
   mainWindow.loadUrl('file://' + __dirname + '/static/index.html');
