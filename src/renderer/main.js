dc6a23d3415f0add68058f308190c1f5656ec879
lemonpole <srivera.x1@gmail.com>
2015-07-15 00:06:36 -0400

    added react to renderer process

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,1 +1,10 @@
+var React = require('react');
+var Sample = React.createClass({
+  render: function(){
+    return(
+      <h1>Hello, world.</h1>
+    );
+  }
+});
 
+React.render(<Sample />, document.getElementById('app'));
