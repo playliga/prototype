ef09325d102ff299e7a207e6711ae762b060ebef
Steve Rivera <srivera.x1@gmail.com>
2015-08-06 16:26:59 -0400

    added dbsetup players

---
+++ src/renderer/dbsetup/northamerica/premier.js
@@ -0,0 +1,298 @@
+var teamArr = [
+  {
+    name: 'Team 34', tag: 'team34 ', country: 'CA', squad: [
+      { username: 'dun1t', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'thetoy', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'TITAN', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'recoil', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'nolimit', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: '8ball', tag: '( 8 ) ', country: 'US', squad: [
+      { username: 'kspot', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'LoughtoN', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: '1ekim', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'elo', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'broodex', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'adverse Reality', tag: 'a|2.', country: 'CA', squad: [
+      { username: 'hustler', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Fierce', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'crock', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'soul', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'cobalt', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'aGather', tag: 'aG ', country: 'CA', squad: [
+      { username: 'paradox', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Crypt1c', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'spectrum', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'byford[nt]', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'fo_sho', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Arsenal', tag: '[ASNL] ', country: 'CA', squad: [
+      { username: 'infect', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'SkYlinE', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'hiERO', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'acecww', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'hypnotic', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Bete Noire', tag: 'BeteNoire ! ', country: 'US', squad: [
+      { username: 'Capt', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: '0rang3', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'CWaLkA', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'hopE!', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'onetime', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Cant Agree', tag: 'team.ca * ', country: 'US', squad: [
+      { username: 'malone', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Synx', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'MaCkAttAcK', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'Slipknot', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'epix', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Confederacy', tag: 'Co :: ', country: 'US', squad: [
+      { username: 'Nitrous', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'cre', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'severity', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'nicKerfoo', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'briski', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'FabricatedX', tag: 'FX ', country: 'US', squad: [
+      { username: 'stu', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'froG', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'diesel', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'ortolaNo', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'zyphh', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Formula 1', tag: 'F1 ', country: 'US', squad: [
+      { username: 'Luckmaster', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'capr1ce', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Tubba', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'zenoirl', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'jamazing', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'From the Ashes', tag: 'fta-', country: 'US', squad: [
+      { username: 'hyeng', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'tehbaitfish', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'ZuLaN', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'meanbean', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'pny', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Gambino', tag: '[G]-', country: 'US', squad: [
+      { username: 'Paulie', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'jamz', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'greddy', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'winn', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'mcvizzLE', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Glimpse of Reality', tag: '[GR] ', country: 'US', squad: [
+      { username: 'odt', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'gurbz', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'vehemens', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'ruke', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'PineKone', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'High-Tech Performance', tag: 'htp ', country: 'US', squad: [
+      { username: 'han', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'nthem', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'jspec', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'sky', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'Swamp_Fox', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Jaded Karma', tag: 'jk | ', country: 'CA', squad: [
+      { username: 'barton', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'bowZer', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'bic', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'NightFaLL', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'mattyz', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'MamazKillaZ', tag: 'mk^', country: 'US', squad: [
+      { username: 'BuDDaH', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'JoHnNy*GM', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'dster', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'sm0og1er', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Iza', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'MissedMe', tag: 'MissedMe|', country: 'US', squad: [
+      { username: 'cruel', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'illuzion', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'rjd2', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'robo', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'm4gic', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Netfragz', tag: 'netfragz | ', country: 'CA', squad: [
+      { username: 'tOyMaCHiN3', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'happa', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'xelleman', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'pwnson', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'd o t t i e', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'NexLevel', tag: 'NexLevel ', country: 'US', squad: [
+      { username: 'jman', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Karpov', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Cooper', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'susan', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'worfless', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'No Such Agency', tag: 'nsa| ', country: 'US', squad: [
+      { username: 'MaKiNaToR', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'chump', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Affliction', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'hydro', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'C-UNNIT[sz]', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'No Talent', tag: '[not] ', country: 'US', squad: [
+      { username: 'phokus', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'ricco', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'kyo', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'shoutZ', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'peck', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Reign Over All', tag: 'RoA ', country: 'US', squad: [
+      { username: 'zeus', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'phaZe-x1-', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'haXoNirL', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'fromage', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'icy[st]', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'ruthless', tag: 'ruthless| ', country: 'US', squad: [
+      { username: 'bestk', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'donks', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'scorp', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'hvk-', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'rez', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'The Final Stand', tag: 'TFS | ', country: 'CA', squad: [
+      { username: 'Jorgenson', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'extasy-', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'fye-fried', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'liquid[pc]', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'twizzy_pc', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Toysoldiers', tag: 'ts - ', country: 'US', squad: [
+      { username: 'fgm', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'sunnybunny', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'ceLica', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'trigerz', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'TehJoe', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'vida', tag: 'vida | ', country: 'US', squad: [
+      { username: 'automat0r', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'seizurE', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'H1kirA', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'CoMeT', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'tr4gik', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'viGor', tag: 'viGor ', country: 'US', squad: [
+      { username: 'fr3aK', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'sMaLLz', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'foomy', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'mbs', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'dsingh', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'inevitable Demise', tag: 'iDEMISE * ', country: 'US', squad: [
+      { username: 'aKASHptl', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'supply', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Des1re-x1', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'KaLIS', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'phamtasitikk', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'pernicious Vengeance', tag: '.pV|', country: 'US', squad: [
+      { username: 'v1rus', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'iiq', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'dim0rph1sm_', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'huLLi', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'narc_', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Riot-Squad', tag: '[rs] ', country: 'CA', squad: [
+      { username: 'icesalmon', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Redeemer', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'sickness', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'Pulse-x1', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'triGGa_', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'Strongpoint', tag: 'spt ', country: 'US', squad: [
+      { username: 'heatsink', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'ecurb', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'm4k', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'spayz', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'bobasaur', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  },
+  {
+    name: 'phatfish', tag: 'phatfish ', country: 'US', squad: [
+      { username: 'tmass', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'nagchampa', skillTemplate: 'VeryHard', weaponTemplate: 'Rifle' },
+      { username: 'Zep', skillTemplate: 'VeryHard', weaponTemplate: 'Sniper' },
+      { username: 'LeNaNe', skillTemplate: 'Hard', weaponTemplate: 'Rifle' },
+      { username: 'mouz', skillTemplate: 'Hard', weaponTemplate: 'Rifle' }
+    ]
+  }
+];
+
+var DBSetupUtil = require('../../utils/DBSetupUtil');
+
+module.exports = {
+  init: function() {
+    return DBSetupUtil.doWork( [ 'US', 'CA' ], teamArr ); 
+  }
+};
