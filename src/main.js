5950a45d6a09b9498e8950ac088d6cc5ded0dc60
lemonpole <srivera.x1@gmail.com>
2015-08-14 00:04:56 -0400

    added dependencies and basic code for web crawler on browser process

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ var app = require('app');
 var BrowserWindow = require('browser-window');
 var mainWindow = null; // prevent window being GC'd
 
-// report crashes to electron
 require('crash-reporter').start();
 
-// when the main window is closed
+// listen to application events
 app.on('window-all-closed', function() {
   // when closing, OSX has some issues so we manually close it below
   if (process.platform != 'darwin') {
@@ -13,14 +12,34 @@ app.on('window-all-closed', function() {
   }
 });
 
-// this is our main app window
-// you can specify different properties for it when instantiating
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600, center: true});
-  mainWindow.loadUrl('file://' + __dirname + '/static/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/renderer/static/index.html');
   mainWindow.openDevTools();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+});
+
+// -----------------------------------------------
+//
+// -----------------------------------------------
+
+var request = require( 'request' );
+var cheerio = require( 'cheerio' );
+var $ = null;
+
+var loadPagePromise = new Promise( function( resolve, reject ) {
+  return request( 'http://google.com', function( error, response, body ) {
+    if( !error && response.statusCode == 200 ) resolve( body );
+    else reject( error );
+  });
+});
+
+loadPagePromise.then( function( body ) {
+  $ = cheerio.load( body );
+  process.stdout.write( $( '#xjsi', 'html' ).html() );
+}).catch( function( error ) {
+  process.stdout.write( error );
 });
--- src/renderer/dbsetup/northamerica/main.js
+++ src/renderer/dbsetup/northamerica/main.js
@@ -2,7 +2,7 @@ var fs = remote.require( 'fs' );
 var fileDataPromises = [];
 
 fileDataPromises.push( new Promise( function( resolve, reject ) {
-  fs.readFile( 'resources/app/static/data/na_main_teams.json', 'utf8', function( err, data ) {
+  fs.readFile( 'resources/app/renderer/static/data/na_main_teams.json', 'utf8', function( err, data ) {
     if( err ) reject( err );
     else resolve( data );
   });
