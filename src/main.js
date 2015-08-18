8fb922189017ddd0ec1b474b493aff6d819e51b2
lemonpole <srivera.x1@gmail.com>
2015-08-18 08:00:09 -0400

    code refactor

--- src/main.js
+++ src/main.js
@@ -31,13 +31,31 @@ var cheerio = require( 'cheerio' );
 var fs = require( 'fs' );
 
 // create array of regions and their division urls
+var BASE_URL = 'https://play.esea.net/index.php?s=league&d=standings&division_id=';
 var regions = {
-  'na': [
-    'https://play.esea.net/index.php?s=league&d=standings&division_id=2490', // professional
-    'https://play.esea.net/index.php?s=league&d=standings&division_id=2491' // premier
-  ],
-  'eu': [
-    'https://play.esea.net/index.php?s=league&d=standings&division_id=2485', // professional
-    'https://play.esea.net/index.php?s=league&d=standings&division_id=2505'
-  ]
+  'na': [ '2490', '2491' ], // professional, premier, ..., open
+  'eu': [ '2485', '2505' ]
 };
+
+// each iteration should return a promise and add it to an array
+for( var region in regions ) {
+  var divisions = regions[ region ];
+  
+  // each iteration should return a promise and add it to an array
+  for( var i = 0; i < divisions.length; i++ ) {
+    var divisionID = divisions[ i ];
+
+    wget( BASE_URL + divisionID ).then( function( data ) {
+      // do work
+    });
+  }
+}
+
+function wget( url ) {
+  return new Promise( function( resolve, reject ) {
+    request( url, function( error, response, data ) {
+      if( !error && response.statusCode == 200 ) resolve( data );
+      else reject( error );
+    });
+  })
+}
