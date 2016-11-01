7aed6cbfba20ff3596f8b2b6d970419f8a67d78a
Steve Rivera <srivera.x1@gmail.com>
2016-11-01 08:28:23 -0400

    create cache dir if not already exists and some code refactor

--- scripts/squad-updater/squad-updater.js
+++ scripts/squad-updater/squad-updater.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+import path from 'path';
+import fs from 'fs';
+
 const BASE_URL = 'https://play.esea.net';
 const DIVISION_URL = `${BASE_URL}/index.php?s=league&d=standings&division_id=`;
 const REGIONS = {
@@ -5,19 +9,57 @@ const REGIONS = {
   eu: [ '2485', '2505' ]
 };
 
+const CACHE_DIR = path.join( __dirname, './cache' );
+
+/*
+* Create cache directory if not already exists. It's fine to block execution
+* as we'd do the same thing if we were to do it async.
+*/
+function cacheDirCheck() {
+  console.log( 'checking if cache directory exists...' );
+
+  if( !fs.existsSync( CACHE_DIR ) ) {
+    console.log( 'cache directory not found. creating...' );
+    fs.mkdirSync( CACHE_DIR );
+  }
+
+  console.log( 'Done.' );
+}
+
+/*
+* Each division has a landing page we need to fetch so that it can then be
+* loaded as an html string.
+*
+* After the pages are fetched they should be cached. Subsequent instantiations
+* should load the html from the cache rather than hitting the web page directly.
+*
+* Unless told otherwise...
+*/
+function fetchDivisionPage( url ) {
+  // Do we have a cache to load from?
+}
+
 /*
 * Couple of things to do here:
-* 1. Loop through each region and its divisions. Visit each division page
-* and extract all of the team URLs.
-*   a. Save a copy of each page to cache so that we are not spamming the server
-*   b. Allow overidding of caching function to visit page directly instead
-*   c. Next time function is ran, check cache instead.
-*
-* 2. Visit each team's page and extract the necessary information such as
-* country, rosters, team name, etc.
+* 1. Loop through each region and its divisions. Fetch each division page
+* and extract all of the team URLs
 */
 function init() {
-  console.log( 'ready to go!' );
+  let region;
+  let divisionId;
+  let html;
+
+  // create cache directory if it does not already exist
+  cacheDirCheck();
+
+  Object.keys( REGIONS ).map( async ( regionId ) => {
+    region = REGIONS[ regionId ];
+
+    for( let i = 0; i < region.length; i++ ) {
+      divisionId = region[ i ];
+      html = await fetchDivisionPage( DIVISION_URL + divisionId );
+    }
+  });
 }
 
 export default {
