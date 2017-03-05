9a07ae0111aa21c14716230fc62d3dfec0644eb1
Steve Rivera <srivera.x1@gmail.com>
2017-03-05 16:43:54 -0500

    convenience npm script to run both electron and webpack server in one

--- webpack/server.js
+++ webpack/server.js
@@ -4,26 +4,48 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack-dev.js';
+import { spawn } from 'child_process';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack( config );
+const argv = require ( 'minimist' )( process.argv.slice( 2 ) );
 
 // inject the webpack middleware modules into the server
 app.use( webpackDevMiddleware( compiler, {
-  noInfo    : true,
+  noInfo: true,
   publicPath: config.output.publicPath,
-  stats     : 'errors-only'
+  stats: 'errors-only'
 }));
 
 app.use( webpackHotMiddleware( compiler ) );
 
 // start the server!
-app.listen( PORT, 'localhost', err => {
+const server = app.listen( PORT, 'localhost', err => {
   if( err ) {
     console.error( err );
     return;
   }
 
-  console.log(`Listening at http://localhost:${PORT}`);
+  // are we also starting electron?
+  if( argv[ 'dev-electron' ] ) {
+    spawn( 'npm', [ 'run', 'start:dev-electron' ], {
+      shell: true,
+      env: process.env,
+      stdio: 'inherit'
+    })
+    .on( 'close', code => process.exit( code ) )
+    .on( 'error', spawnError => console.error( spawnError ) );
+  }
+
+  console.log( `Listening at http://localhost:${PORT}` );
+});
+
+// we need to handle exiting the server since we're spawning a process above
+// depending on the arguments passed
+process.on( 'SIGTERM', () => {
+  console.log( 'Stopping dev server' );
+
+  wdm.close();
+  server.close(() => process.exit( 0 ) );
 });
