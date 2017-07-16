35eafd3f9020392c68f3950eb8d1d50c68e17e6a
Steve Rivera <srivera.x1@gmail.com>
2017-07-16 14:36:45 -0400

    simple animation for active item

--- renderer-process/routes/home/home.jsx
+++ renderer-process/routes/home/home.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import styles from './home.scss';
 
+type State = {
+  activeItem: number | null
+}
+
 export default class Home extends Component {
+  state: State
+
+  state = {
+    activeItem: null
+  }
+
   render() {
+    const { activeItem } = this.state;
+
     return (
       <div className={styles.container}>
         {[ 'New Career', 'Load Career' ].map( ( item, i ) => {
           const menuImage = require( `assets/images/home-splash-${i}.jpg` ); // eslint-disable-line
+          const activeStyle = ( activeItem !== null && activeItem === i ) || activeItem === null ?
+            styles.active : styles.inactive;
 
           return (
             <div
               key={i}
-              className={styles.item}
+              className={`${styles.item} ${activeStyle}`}
               style={{ backgroundImage: `url(${menuImage})` }}
+              onClick={() => this.setState({ activeItem: i })}
             >
               <h1>{item}</h1>
             </div>
