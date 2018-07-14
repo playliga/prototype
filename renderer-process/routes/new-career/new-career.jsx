9a96a9211098cd0a0d2d43b8bbfb5fb3cc5ead65
Steve Rivera <srivera.x1@gmail.com>
2018-07-14 12:56:44 -0400

    add notes to new-career page

--- renderer-process/routes/new-career/new-career.jsx
+++ renderer-process/routes/new-career/new-career.jsx
@@ -1,7 +1,14 @@
 // @flow
 import React from 'react';
 
-
+/**
+ * This route will have nested routes which will represent the
+ * three forms:
+ *
+ * - User information
+ * - Team information
+ * - Starting V (technically 4 because user is included)
+ */
 const NewCareer = () => (
   <h1 style={{ color: 'red' }}>{'Hey!'}</h1>
 );
