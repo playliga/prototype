85942bb00416ceb85912cee157db62d3f41b6051
Steve Rivera <srivera.x1@gmail.com>
2018-07-21 15:20:55 -0400

    move form up a level and apply className. add styling for squad page

--- renderer-process/routes/new-career/new-career.jsx
+++ renderer-process/routes/new-career/new-career.jsx
@@ -10,7 +10,7 @@ const validate = value => (
 );
 
 /**
- * This route will have nested routes which will represent the
+ * This route will have nested routes which represent the
  * three forms:
  *
  * - User information
@@ -18,144 +18,156 @@ const validate = value => (
  * - Starting V (technically 4 because user is included)
  */
 const Squad = () => (
-  <h2 className={styles.subtitle}>
-    {'Squad'}
-  </h2>
+  <section className={styles.container}>
+    <section className={styles.content}>
+      <h2 className={styles.subtitle}>
+        {'Squad'}
+      </h2>
+    </section>
+
+    <Form id="squad" className={styles.wideContent}>
+      {'Hey'}
+    </Form>
+  </section>
 );
 
 const TeamInformation = props => (
-  <Form
-    id="team"
-    onSubmit={() => props.history.push( '/new-career/squad', { title: 'New Career' })}
-  >
-    {({ formState }) => (
-      <Fragment>
-        <h2 className={styles.subtitle}>
-          {'Team Information'}
-        </h2>
+  <section className={styles.container}>
+    <Form
+      id="team"
+      className={styles.content}
+      onSubmit={() => props.history.push( '/new-career/squad', { title: 'New Career' })}
+    >
+      {({ formState }) => (
+        <Fragment>
+          <h2 className={styles.subtitle}>
+            {'Team Information'}
+          </h2>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="tname"
-            id="tname"
-            placeholder="Name"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="tname"
+              id="tname"
+              placeholder="Name"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="tag"
-            id="tag"
-            placeholder="Tag"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="tag"
+              id="tag"
+              placeholder="Tag"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="country"
-            id="country"
-            placeholder="Country"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="country"
+              id="country"
+              placeholder="Country"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <button
-          type="submit"
-          className={styles.submit}
-          disabled={
-            formState.invalid
-            || formState.pristine
-            || Object.keys( formState.values ).length !== 3
-          }
-        >
-          {'Submit'}
-        </button>
-      </Fragment>
-    )}
-  </Form>
+          <button
+            type="submit"
+            className={styles.submit}
+            disabled={
+              formState.invalid
+              || formState.pristine
+              || Object.keys( formState.values ).length !== 3
+            }
+          >
+            {'Submit'}
+          </button>
+        </Fragment>
+      )}
+    </Form>
+  </section>
 );
 
 
 const PlayerInformation = props => (
-  <Form
-    id="player"
-    onSubmit={() => props.history.push( '/new-career/team', { title: 'New Career' })}
-  >
-    {({ formState }) => (
-      <Fragment>
-        <h2 className={styles.subtitle}>
-          {'Player Information'}
-        </h2>
+  <section className={styles.container}>
+    <Form
+      id="player"
+      className={styles.content}
+      onSubmit={() => props.history.push( '/new-career/team', { title: 'New Career' })}
+    >
+      {({ formState }) => (
+        <Fragment>
+          <h2 className={styles.subtitle}>
+            {'Player Information'}
+          </h2>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="fname"
-            id="fname"
-            placeholder="First name"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="fname"
+              id="fname"
+              placeholder="First name"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="lname"
-            id="lname"
-            placeholder="Last name"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="lname"
+              id="lname"
+              placeholder="Last name"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="alias"
-            id="alias"
-            placeholder="Alias"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="alias"
+              id="alias"
+              placeholder="Alias"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <div className={styles.fieldSet}>
-          <Text
-            field="country"
-            id="country"
-            placeholder="Country"
-            validateOnChange
-            validate={validate}
-          />
-        </div>
+          <div className={styles.fieldSet}>
+            <Text
+              field="country"
+              id="country"
+              placeholder="Country"
+              validateOnChange
+              validate={validate}
+            />
+          </div>
 
-        <button
-          type="submit"
-          className={styles.submit}
-          disabled={
-            formState.invalid
-            || formState.pristine
-            || Object.keys( formState.values ).length !== 4
-          }
-        >
-          {'Submit'}
-        </button>
-      </Fragment>
-    )}
-  </Form>
+          <button
+            type="submit"
+            className={styles.submit}
+            disabled={
+              formState.invalid
+              || formState.pristine
+              || Object.keys( formState.values ).length !== 4
+            }
+          >
+            {'Submit'}
+          </button>
+        </Fragment>
+      )}
+    </Form>
+  </section>
 );
 
 const NewCareer = () => (
-  <section className={styles.container}>
-    <section className={styles.content}>
-      <Route exact path="/new-career" component={PlayerInformation} />
-      <Route exact path="/new-career/team" component={TeamInformation} />
-      <Route exact path="/new-career/squad" component={Squad} />
-    </section>
-  </section>
+  <Fragment>
+    <Route exact path="/new-career" component={PlayerInformation} />
+    <Route exact path="/new-career/team" component={TeamInformation} />
+    <Route exact path="/new-career/squad" component={Squad} />
+  </Fragment>
 );
 
 export default NewCareer;
