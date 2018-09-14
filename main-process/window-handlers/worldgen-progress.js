3c360b7d15db7a0d29c9defea12dc9453c3a3886
Steve Rivera <srivera.x1@gmail.com>
2018-09-13 22:35:35 -0400

    move some things around

--- main-process/window-handlers/worldgen-progress.js
+++ main-process/window-handlers/worldgen-progress.js
@@ -2,44 +2,48 @@
 import path from 'path';
 import { ipcMain } from 'electron';
 import ProgressBar from 'electron-progressbar';
-
 import { ScraperFactory } from '../../common/scraper-factory';
 
 
 const CACHE_DIR = path.join( __dirname, 'cache' );
+const WIN_OPTS = {
+  text: 'Creating world.',
+  detail: 'Generating free agents...',
+  browserWindow: {
+    titleBarStyle: 'hidden'
+  }
+};
 
 function generateFreeAgents(): Promise<Object> {
-  const factoryObj = new ScraperFactory( CACHE_DIR, 'esea-csgo-freeagents' );
-  return factoryObj.generate();
+  return new ScraperFactory(
+    CACHE_DIR, 'esea-csgo-freeagents'
+  ).generate();
 }
 
 function generateTeamsAndPlayers(): Promise<Array<Object>> {
-  const factoryObj = new ScraperFactory( CACHE_DIR, 'esea-csgo' );
-  return factoryObj.generate();
+  return new ScraperFactory(
+    CACHE_DIR, 'esea-csgo'
+  ).generate();
+}
+
+function ipcHandler( event: Object, data: Array<Object> ): void {
+  // close the current window
+  // create a new window that generates the world (season, leagues, etc)
+  const win = new ProgressBar( WIN_OPTS );
+
+  generateFreeAgents()
+    .then( ( res: Object ) => {
+      win.detail = 'Generating teams and players...';
+      return generateTeamsAndPlayers();
+    })
+    .then( ( res: Array<Object> ) => {
+      console.log( res );
+      win.detail = 'Generating leagues...';
+    })
+    .catch( err => console.log( err ) )
+    .finally( () => setTimeout( () => win.setCompleted(), 5000 ) );
 }
 
 export default () => {
-  ipcMain.on( 'new-career', ( event: Object, data: Array<Object> ) => {
-    // close the current window
-    // create a new window that generates the world (season, leagues, etc)
-    const win = new ProgressBar({
-      text: 'Creating world.',
-      detail: 'Generating free agents...',
-      browserWindow: {
-        titleBarStyle: 'hidden'
-      }
-    });
-
-    generateFreeAgents()
-      .then( ( res: Object ) => {
-        win.detail = 'Generating teams and players...';
-        return generateTeamsAndPlayers();
-      })
-      .then( ( res: Array<Object> ) => {
-        console.log( res );
-        win.detail = 'Generating leagues...';
-      })
-      .catch( err => console.log( err ) )
-      .finally( () => setTimeout( () => win.setCompleted(), 5000 ) );
-  });
+  ipcMain.on( 'new-career', ipcHandler );
 };
