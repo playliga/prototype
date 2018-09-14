d6e800f96854c57a109dc93ca0cf8823c3715130
Steve Rivera <srivera.x1@gmail.com>
2018-09-13 21:11:29 -0400

    screape teams and players

--- main-process/window-handlers/worldgen-progress.js
+++ main-process/window-handlers/worldgen-progress.js
@@ -6,10 +6,15 @@ import ProgressBar from 'electron-progressbar';
 import { ScraperFactory } from '../../common/scraper-factory';
 
 
-function generateFreeAgents(): Promise<Object> {
-  const CACHE_DIR = path.join( __dirname, 'cache' );
-  const factoryObj = new ScraperFactory( CACHE_DIR, 'esea-csgo-freeagents' );
+const CACHE_DIR = path.join( __dirname, 'cache' );
 
+function generateFreeAgents(): Promise<Object> {
+  const factoryObj = new ScraperFactory( CACHE_DIR, 'esea-csgo-freeagents' );
+  return factoryObj.generate();
+}
+
+function generateTeamsAndPlayers(): Promise<Array<Object>> {
+  const factoryObj = new ScraperFactory( CACHE_DIR, 'esea-csgo' );
   return factoryObj.generate();
 }
 
@@ -18,7 +23,7 @@ export default () => {
     // close the current window
     // create a new window that generates the world (season, leagues, etc)
     const win = new ProgressBar({
-      text: 'Creating world...',
+      text: 'Creating world.',
       detail: 'Generating free agents...',
       browserWindow: {
         titleBarStyle: 'hidden'
@@ -27,8 +32,12 @@ export default () => {
 
     generateFreeAgents()
       .then( ( res: Object ) => {
-        console.log( res );
         win.detail = 'Generating teams and players...';
+        return generateTeamsAndPlayers();
+      })
+      .then( ( res: Array<Object> ) => {
+        console.log( res );
+        win.detail = 'Generating leagues...';
       })
       .catch( err => console.log( err ) )
       .finally( () => setTimeout( () => win.setCompleted(), 5000 ) );
