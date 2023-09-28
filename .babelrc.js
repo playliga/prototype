6d67437d0020642180a7ff927ee7e4d6080e1d80
lemonpole <srivera.x1@gmail.com>
2023-09-28 12:03:35 -0400

    build: fix babel transpile errors

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,21 @@
 module.exports = {
-  "presets": [
-    ["@babel/env", {
-      "targets": {
-        "node": true
-      }
-    }],
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: true,
+        },
+      },
+    ],
     "@babel/typescript",
     "@babel/react",
   ],
-  "plugins": [
+  plugins: [
     "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
     "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-transform-runtime"
-  ]
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-transform-runtime",
+  ],
 };
