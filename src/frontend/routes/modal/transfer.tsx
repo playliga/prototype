5e84bb9b17c7c8f7ced62811f4b32653972bffff
lemonpole <lemonpole@users.noreply.github.com>
2025-05-08 21:24:23 -0400

    build: upgrade prettier

--- src/frontend/routes/modal/transfer.tsx
+++ src/frontend/routes/modal/transfer.tsx
@@ -269,7 +269,10 @@ export default function () {
               const { value, max } = xp.normalize(stat);
 
               return (
-                <td key={`xp__${player.name}_${stat}_value`} className="border-l border-base-content/10">
+                <td
+                  key={`xp__${player.name}_${stat}_value`}
+                  className="border-l border-base-content/10"
+                >
                   <XPBar
                     title={`${startCase(stat)}`}
                     subtitle={`${value}/${max}`}
