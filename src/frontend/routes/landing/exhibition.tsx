e780bb5bcc42d34777cfbe7fed00429cf08f92ad
lemonpole <srivera.x1@gmail.com>
2026-06-18 17:05:07 -0400

    fix: fix issues detecting game path in exhibition mode (#621)

--- src/frontend/routes/landing/exhibition.tsx
+++ src/frontend/routes/landing/exhibition.tsx
@@ -243,7 +243,7 @@ export default function () {
 
     // detect game path again
     api.app.detectGame(modified.general.game).then((gamePath) => {
-      modified.general.gamePath = gamePath;
+      modified.general.gamePath = gamePath || modified.general.gamePath;
       setSettings(modified);
     });
 
